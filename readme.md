15d227fd6ea455facf324568f1223a4271d09fb3
elliotwoods <elliot.woods@gmail.com>
2012-06-19 13:22:11 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+# ofxTriangulate
+
+## Introduction
+
+Find 3D vertices given:
+
+* An ofxGraycode::DataSet
+* An ofxRay::Camera
+* An ofxRay::Projector
+
+## Prerequisites
+
+* ofxRay
+* ofxGraycode
---
+++ src/ofxTriangulate.h
@@ -0,0 +1,12 @@
+#pragma once
+//
+//  ofxTriangulate.h
+//  ofxTriangulate
+//
+//  Elliot Woods (C) 2012, MIT license
+//	http://www.kimchiandchips.com
+//
+
+//include me in your project
+//and i'll include everything else
+
